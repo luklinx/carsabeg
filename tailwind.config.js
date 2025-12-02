2d98c5b122a03e3c0242b86fdf9e0f3eaa10e627
luklinx <lukvlgsl@gmail.com>
2025-12-02 23:48:32 +0100

    Hero section changes took place

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        "grid-green-900": `
+          linear-gradient(to right, #1B4332 1px, transparent 1px),
+          linear-gradient(to bottom, #1B4332 1px, transparent 1px)
+        `,
+      },
+    },
+  },
+};
